cfc1fe46a5dcf53574544a9645eccea300a30174
tonyadms1509 <tonyadams1509@gmail.com>
2026-03-15 20:55:56 +0200

    Wrap App in BrowserRouter for routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // ✅ add router
 import App from './App';
 import './index.css'; // optional, only if you have styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
