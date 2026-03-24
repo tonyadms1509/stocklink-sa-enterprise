a5bbf6f5fed21246f248d7cab130af21987e97a5
tonyadms1509 <tonyadams1509@gmail.com>
2026-03-24 11:02:29 +0200

    Create App.tsx without landing page

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import Admin from './pages/admin/Admin';
+import Contractor from './pages/contractor/Contractor';
+import SupplierProfile from './pages/supplier/SupplierProfile';
+import LogisticsProfile from './pages/logistics/LogisticsProfile';
+import Driver from './pages/driver/Driver';
+
+function App() {
+  return (
+    <Router>
+      {/* Persistent navigation bar */}
+      <NavBar />
+
+      {/* Route definitions */}
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/contractor" element={<Contractor />} />
+        <Route path="/supplier" element={<SupplierProfile />} />
+        <Route path="/logistics" element={<LogisticsProfile />} />
+        <Route path="/driver" element={<Driver />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
